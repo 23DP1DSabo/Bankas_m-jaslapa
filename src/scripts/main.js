268d46a586c1a57c2af960bd54d6cc69e5deb7ef
Daniels Šabovičs <A230158DS@rvt.lv>
2025-04-16 00:02:53 +0300

    Made a crude mobile view responsivity.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,21 @@
- // some epic javascript code 
-// that does some epic stuff
+const toggleBtn = document.querySelector(".menu-toggle");
+const mobileMenu = document.getElementById("mobileMenu");
+const overlay = document.getElementById("overlay");
+
+toggleBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+const links = mobileMenu.querySelectorAll("a");
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
